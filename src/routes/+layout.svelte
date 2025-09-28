5c35fb97e40075da65a173f0763ab50e9f250b40
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-09-28 17:10:51 +0900

    chore: svelte-head title repro

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<title>Hello, World!</title>
 </svelte:head>
 
 {@render children?.()}
